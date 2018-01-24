e1d99e78c6d2b4163d94da80d1f67eb043f53fd4
Johannes Ahnlide <johannes@voxel.se>
2018-01-24 17:08:05 +0100

    Restructuring and command line argument parsing
    - Prepared for separation of scoring and calculation
    - Added argparse with loglevel, seed and iteration count arguments

--- main.py
+++ main.py
@@ -1,41 +1,66 @@
 from score import score
-import sys
 import random
 from random import randint as ri
 import logging as log
-log.basicConfig(level=log.DEBUG, format='%(relativeCreated)6d %(message)s')
-
-testcase = sys.argv[1]
-
-with open('in/' + testcase + '.in') as f:
-    lines = f.readlines()
-### TODO: Proccess input data ###
+import argparse
 
 
 # Runs scoring function and checks if score is improved.
 def process(out, seed):
-    sc = score(out)
-    f = open(testcase+ '.max', 'r')
-    bsc = int(f.readline())
-    f.close()
+    sc = score(inp, out)
+
+    with open(args.testcase + '.max', 'r') as f:
+        bsc = int(f.readline())
+
     if sc > bsc:
-        log.critical('New best score {} for testcase {}'.format(sc, testcase))
-        fname = '_'.join([testcase, str(sc), seed]) + '.ans'
-        f = open(testcase + '.max', 'w')
-        f.write(str(sc))
-        f.close()
+        log.critical('New best score {} for testcase {}'.format(sc, args.testcase))
+        fname = '_'.join([args.testcase, str(sc), seed]) + '.ans'
+
+        with open(args.testcase + '.max', 'w') as f:
+            f.write(str(sc))
+
         with open('ans/' + fname, 'w') as f:
             # Print to f
             f.write(str(out))
     else:
         log.warn('Score: {}'.format(sc))
 
+
 def greedy(seed):
-    #TODO: Solve the problem
+    # TODO: Solve the problem
+    random.seed(seed)
 
     process(0, seed)
 
-for i in range(10):
-    seed = random.randint(0, 10000)
-    log.info('seed: {}, test#: {}'.format(seed, i))
-    greedy(seed)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('testcase')
+    parser.add_argument('-l', '--log', default='debug')
+    parser.add_argument('-s', '--seed', default=None)
+    parser.add_argument('-n', '--iterations', default=10)
+    return parser.parse_args()
+
+
+def init_log():
+    loglvls = {'debug': log.DEBUG, 'info': log.INFO, 'warning': log.WARNING, 'error': log.ERROR, 'critical': log.CRITICAL}
+    logfmt = '%(relativeCreated)6d %(message)s %(arg.testcase)s'
+    log.basicConfig(level=loglvls[args.log], format=logfmt)
+
+
+if __name__ == '__main__':
+    args = get_args()
+    init_log()
+
+    with open('in/' + args.testcase + '.in') as f:
+        inp = f.read()
+        # TODO: Proccess input data
+
+    if args.seed:
+        log.info('seed: {}'.format(args.seed))
+        greedy(args.seed)
+    else:
+        for i in range(args.iterations):
+            seed = ri(0, 10000)
+            log.info('seed: {}, test#: {}'.format(seed, i))
+            greedy(seed)
