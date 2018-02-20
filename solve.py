4423da181891ed0402cdc101f42a7b031901cda8
Måns Magnusson <exoji2e@gmail.com>
2018-02-20 23:30:42 +0100

    add functions and -s arg that we will need in solve

--- solve.py
+++ solve.py
@@ -2,22 +2,41 @@ import argparse
 import random
 
 
-def score(inp, out):
+def parse(inp):
     # TODO: implement
-    return 0
+    itr = (map(int, li.split()) for li in inp.split('\n'))
+    r, c = next(itr)
+
+    return argparse.Namespace(r=r, c=c)
 
 
 def solve(seed, inp, log):
     # TODO: Solve the problem
     random.seed(seed)
+    ns = parse(inp)
 
     return '0'
+
+
+def show(out):
+    # TODO: Print the solution here
+    print(out)
+
+
+def score(inp, out):
+    # TODO: implement
+
+    if __name__ == '__main__' and args.s:
+        show(out)
+
+    return 0
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('inp')
     parser.add_argument('ans')
+    parser.add_argument('-s', action='store_true', help="show")
     return parser.parse_args()
 
 
