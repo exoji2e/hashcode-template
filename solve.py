785431e1ad9d80e72c1604b4c7642a98f58f11b2
Måns Magnusson <exoji2e@gmail.com>
2018-02-28 23:12:11 +0100

    Handle if only one file is given to solve.py
    solve.py tries to detirmine if one file is an
    input or ans-file. If zero files are given as
    arguments, solve.py will be run on every .ans-file
    inside the submission directory.

--- solve.py
+++ solve.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env pypy2
 import argparse
 import random
+import glob
 
 
 def parse(inp):
@@ -34,24 +36,45 @@ def score(inp, out):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-inp', default='')
-    parser.add_argument('-ans', default='')
+    parser.add_argument('inp', nargs='?')
+    parser.add_argument('ans', nargs='?')
     parser.add_argument('-s', action='store_true', help="show")
-    parser.add_argument('-t', action='store', help="will look for in/testcase.in and submission/testcase.ans")
     return parser.parse_args()
+
+
+def ans2in(ans):
+    return ans.replace('.ans', '.in').replace('submission/', 'in/')
+
+
+def in2ans(inp):
+    return inp.replace('.in', '.ans').replace('in/', 'submission/')
 
 
 if __name__ == '__main__':
     args = get_args()
+    if not args.ans:
+        if '.ans' in args.inp:
+            args.ans = args.inp
+            args.inp = ans2in(args.ans)
+        elif '.in' in args.inp:
+            args.ans = in2ans(args.inp)
+        else:
+            args.inp = args.inp.replace('.max', '')
+            args.ans = 'submission/' + args.inp + '.ans'
+            args.inp = 'in/' + args.inp + '.in'
 
-    if args.t:
-        args.inp = 'in/' + args.t + '.in'
-        args.ans = 'submission/' + args.t + '.ans'
+    if not args.inp and not args.ans:
+        files = []
+        for ans in glob.glob('submission/*.ans'):
+            files.append((ans2in(ans), ans))
+    else:
+        files = [(args.inp, args.ans)]
 
-    with open(args.inp, 'r') as f:
-        inp = f.read()
+    for inpf, ansf in files:
+        with open(inpf, 'r') as f:
+            inp = f.read()
+        with open(ansf, 'r') as f:
+            ans = f.read()
 
-    with open(args.ans, 'r') as f:
-        ans = f.read()
-
-    print(score(inp, ans))
+        print('{} {}'.format(inpf, ansf))
+        print(score(inp, ans))
