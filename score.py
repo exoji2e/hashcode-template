46c0d7fb70ca4705cf0bfca62cc8efdda666a351
Måns Magnusson <exoji2e@gmail.com>
2019-02-27 21:40:32 +0100

    add some comments around the score and solve functios

--- score.py
+++ score.py
@@ -16,6 +16,7 @@ def show(out):
 def ni(itr):
     return int(next(itr))
 
+# parses the next string of itr as a list of integers
 def nl(itr):
     return [int(v) for v in next(itr).split()]
 
@@ -27,7 +28,12 @@ def parse(inp):
 
     return ns
 
+# inp: the input file as a single string
+# out: the answer file produced by your solver, as a single string
+# return the score of the output as an integer
 def score(inp, out):
+    ns = parse(inp)
+    itr = (line for line in out.split('\n'))
     # TODO: implement
 
     if __name__ == '__main__' and args.s:
--- solve.py
+++ solve.py
@@ -2,6 +2,8 @@ import argparse
 import random
 from score import parse
 
+# inp is an input file as a single string
+# return your output as a string
 def solve(seed, inp, log):
     # TODO: Solve the problem
     random.seed(seed)
