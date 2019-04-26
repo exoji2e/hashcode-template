068e9ba8fc8e87ecc112d945f8b952f9c022392f
Måns Magnusson <exoji2e@gmail.com>
2019-04-26 15:33:02 +0200

    sum_score now print names of files as well

--- sum_score.py
+++ sum_score.py
@@ -1,5 +1,7 @@
 import glob
 S = 0
 for f in glob.glob('*.max'):
-    S += int(open(f, 'r').read())
-print(S)
+    v = int(open(f, 'r').read())
+    print('{}: {}'.format(f.replace('.max', ''), v))
+    S += v
+print('Total: {}'.format(S))
